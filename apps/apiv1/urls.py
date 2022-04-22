7421e255183883939bf01e0e0f546e0c8b1520f3
yejue <yejue@yjstudy1.com>
2022-04-22 10:44:58 +0800

    [add] 添加补天漏洞盒子信息展示接口

--- apps/apiv1/urls.py
+++ apps/apiv1/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
+from apps.spider.views import BtAndVulBoxDataViewSet
+from libs.routers import SimpleRouter
 
 app_name = "apiv1"
 
-urlpatterns = [
+simple_router = SimpleRouter()
+simple_router.register(BtAndVulBoxDataViewSet)  # 注册补天盒子视图集
 
+urlpatterns = [
 ]
+
+urlpatterns += simple_router.get_urls()
--- apps/spider/views.py
+++ apps/spider/views.py
@@ -1,4 +1,5 @@
-from libs.views import SearchEngineSpiderView
+from libs.views import SearchEngineSpiderView, SimpleViewSet
+from apps.spider.models import BtAndVulBoxModel
 from spider.spiders import BaiduSearchSpider, GoogleSearchSpider
 
 # Create your views here.
@@ -12,3 +13,10 @@ class BaiduSearchSpiderView(SearchEngineSpiderView):
 class GoogleSearchSpiderView(SearchEngineSpiderView):
     """谷歌搜索爬虫视图"""
     spider_class = GoogleSearchSpider
+
+
+class BtAndVulBoxDataViewSet(SimpleViewSet):
+    """补天&漏洞盒子爬虫任务数据展示视图"""
+    operation_fields = ["id", "company_name", "test_range", "property_range", "origin", "origin_url"]
+    model = BtAndVulBoxModel
+    base_name = "bt-vulbox"
