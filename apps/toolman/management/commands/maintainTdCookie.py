8c7cb7451ad744aee1d3c1645def08cc8f5c26e3
yejue <yejue@yjstudy1.com>
2022-04-27 19:28:23 +0800

    [add] 添加 TideFingerSpider Cookie 维护指令

---
+++ apps/toolman/management/commands/maintainTdCookie.py
@@ -0,0 +1,20 @@
+import logging
+
+from django.core.management import BaseCommand
+from spider.spiders import TdFingerSearchSpider
+
+logger = logging.getLogger("spider")  # 爬虫日志器
+
+
+class Command(BaseCommand):
+    """潮汐在线指纹识别 Cookie 维护"""
+    domain = "baidu.com"
+    spider = TdFingerSearchSpider(domain)
+    res = spider.get_finger_data()
+
+    if not res:
+        print("Cookie 维护失败，该 Cookie 已提前过期")
+        logger.info("Cookie 维护失败，该 Cookie 已提前过期")
+    else:
+        print("Cookie 维护完成")
+        logger.info("Cookie 维护完成")
