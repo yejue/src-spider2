a8cd85a21ccf87f69b32eb713a6e6f7ded7dfe33
yejue <yejue@yjstudy1.com>
2022-05-09 23:26:59 +0800

    [add] 添加站长之家 PING 节点爬虫

--- apps/spider/urls.py
+++ apps/spider/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('baidu-search/', views.BaiduSearchSpiderView.as_view(), name="baidu_spider"),
     path('google-search/', views.GoogleSearchSpiderView.as_view(), name="google_spider"),
     path('td-search/', views.TideFingerSearchSpiderView.as_view(), name="td_spider"),
+    path('chinaz-search/', views.ChinaZPingSpiderView.as_view(), name="chinaz-search"),
 ]
--- apps/spider/views.py
+++ apps/spider/views.py
@@ -3,7 +3,11 @@ from django.views import View
 from libs.views import SearchEngineSpiderView, SimpleViewSet
 from libs.response import json_response
 from apps.spider.models import BtAndVulBoxModel
-from spider.spiders import BaiduSearchSpider, GoogleSearchSpider, TdFingerSearchSpider
+
+from spider.spiders import (
+    BaiduSearchSpider, GoogleSearchSpider, TdFingerSearchSpider,
+    ChinaZPingSpider
+)
 
 # Create your views here.
 
@@ -26,12 +30,27 @@ class BtAndVulBoxDataViewSet(SimpleViewSet):
 
 
 class TideFingerSearchSpiderView(View):
-    """潮汐指纹在线爬虫"""
+    """潮汐指纹在线爬虫视图"""
 
     def get(self, request):
         domain = request.GET.get("domain")
+        if domain.strip() is "" or domain is None:
+            return json_response(message="domain 不能为空")
         spider = TdFingerSearchSpider(domain)
         res = spider.get_finger_data()
         if not res:
             return json_response("TideFinger Cookie 失效，请重新设置")
+        return json_response(data=res)
+
+
+class ChinaZPingSpiderView(View):
+    """站长之家 PING 节点爬虫视图"""
+
+    def get(self, request):
+        domain = request.GET.get("domain", None)
+        if domain.strip() is "" or domain is None:
+            return json_response(message="domain 不能为空")
+        spider = ChinaZPingSpider(domain)
+        res = spider.run()
+
         return json_response(data=res)
